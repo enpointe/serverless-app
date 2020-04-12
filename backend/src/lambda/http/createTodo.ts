2e98c7f91ee5d40acff97a948e7d3a78ccd027ce
Kristina Tripp <kristina.tripp@yahoo.com>
2020-04-12 13:34:51 -0600

    feat: Initial impl of createTodo and getTools
    Authorization not implemented

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,41 @@
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing TODO event', event)
+
+  const itemId = uuid.v4()
+  const timestamp = new Date().toISOString()
+  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  console.log(`Timestamp ${timestamp}`)
+  const newItem = {
+    id: itemId,
+    userId: itemId,
+    createdAt: timestamp,
+    done: false,
+    ...newTodo
+  }
+
+  await docClient.put({
+    TableName: todoTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,28 @@
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todoTable = process.env.TODO_TABLE
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  console.log('Processing get TODO event: ', event)
+
+  const result = await docClient.scan({
+    TableName: todoTable
+  }).promise()
+
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
