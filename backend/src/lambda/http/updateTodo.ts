ed9002267349cf762de5a848075a00d8b16192e5
Kristina Tripp <kristina.tripp@yahoo.com>
2020-04-12 14:17:04 -0600

    feat: Added ability to update Todo

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,14 +1,68 @@
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  console.log("Procesing event ", todoId, updatedTodo)
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  console.log("Fetching record")
+  let entry = await docClient.get({
+    TableName: todoTable,
+    Key: {'id': todoId}
+  }).promise()
+  .catch(function(error) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        "error": error
+      })
+    }
+  })
+
+  console.log("Procesing event for id", todoId, updatedTodo)
+  await docClient.put({
+    TableName: todoTable,
+    Item: {
+      id: todoId,
+      ...updatedTodo
+    }
+  }).promise()
+  .catch(function(error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        "error": error
+      })
+    }
+  })
+
+  console.log("Fetching updated record")
+  entry = await docClient.get({
+    TableName: todoTable,
+    Key: {'id': todoId}
+  }).promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      entry
+    })
+  }
   return undefined
 }
