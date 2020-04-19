9c45a718824e7cec1628f5a1cb6345639c14f3bf
Kristina Tripp <kristina.tripp@yahoo.com>
2020-04-19 14:36:46 -0600

    feat: Added certification helper method

---
+++ backend/src/utils/cert.ts
@@ -0,0 +1,8 @@
+/**
+ * From https://github.com/sgmeyer/auth0-node-jwks-rs256/blob/master/src/lib/utils.js
+ */
+export function certToPEM(cert) {
+    cert = cert.match(/.{1,64}/g).join('\n');
+    cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+    return cert;
+  }
